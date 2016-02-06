aaba8483a1321bd01d6e22dcb97dcf1d8bd16957
KhvedY <jillfoh@mail.ru>
2016-02-06 22:25:52 +0300

    Update Bank.cpp

--- Bank.cpp
+++ Bank.cpp
@@ -1,4 +1,6 @@
 #include "Bank.h"
+#include <iostream>
+using namespace std;
 
 Bank::Bank(uint fil_balance, uint balance_all)
 {
@@ -8,7 +10,7 @@ Bank::Bank(uint fil_balance, uint balance_all)
 	Setbalance_all(balance_all);
 }
 
-Bank::Bank(uint deposite, uint t_deposite, uint t_credit, uint g_credit)
+Bank::Bank(uint deposite, uint t_deposite, uint t_credit, uint g_credit,uint val,uint value)
 {
 	this->deposite = 0;
 	this->t_deposite = 0;
@@ -17,14 +19,7 @@ Bank::Bank(uint deposite, uint t_deposite, uint t_credit, uint g_credit)
 	Setdeposite(deposite);
 	Sett_deposite(t_deposite,value);
 	Sett_credit(t_credit);
-	Setg_credit(g_credit,val);
-}
-
-void Bank::Setfil_balance(uint fil_balance)
-{
-	if (fil_balance<balance_all)
-	this->fil_balance = fil_balance;
-	else fil_balance = balance_all;
+	Setg_credit(g_credit, val);
 }
 
 void Bank::Setbalance_all(uint balance_all)
@@ -32,23 +27,51 @@ void Bank::Setbalance_all(uint balance_all)
 	this->balance_all = balance_all;
 }
 
-void Bank::Setdeposite(uint deposite)
+void Bank::Setfil_balance(uint fil_balance)
 {
-	this->deposite = deposite;
+	if (fil_balance<balance_all)
+		this->fil_balance = fil_balance;
+	else fil_balance = balance_all;
 }
 
-void Bank::Sett_deposite(uint t_deposite,uint value)
+void Bank::Setwf_balance(uint wf_balance)
 {
-	this->t_deposite += t_deposite*value/100;
+	if (wf_balance < fil_balance)
+		this->wf_balance = wf_balance;
+	else
+		wf_balance = fil_balance;
+}
+
+void Bank::Setwb_balance(uint wb_balance)
+{
+	if (wb_balance < balance_all)
+		this->wb_balance = wb_balance;
+	else
+		wb_balance = balance_all;
+}
+
+void Bank::Setdeposite(uint deposite)
+{
+	if (deposite < wf_balance)
+		this->deposite = deposite;
+	else
+		cout << "sorry we cant give u deposite :(";
+}
+
+void Bank::Sett_deposite(uint t_deposite, uint value)
+{
+	this->t_deposite += t_deposite*value / 100;
 }
 
 void Bank::Sett_credit(uint credit)
 {
-	if (t_credit < fil_balance||t_credit<balance_all)
-	this->t_credit = credit;
+	if (t_credit < fil_balance || t_credit < balance_all)
+		this->t_credit = credit;
+	else
+		cout << "sorry we can't give u credit :(";
 }
 
 void Bank::Setg_credit(uint g_credit, uint val)
 {
-	this->g_credit += g_credit*val/100;
+	this->g_credit += g_credit*val / 100;
 }
