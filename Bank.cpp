1d9dd2248d95192c100bb675d904047ecf5c24c2
KhvedY <jillfoh@mail.ru>
2016-02-08 02:33:42 +0300

    Update Bank.cpp

--- Bank.cpp
+++ Bank.cpp
@@ -2,30 +2,8 @@
 #include <iostream>
 using namespace std;
 
-Bank::Bank(uint fil_balance, uint balance_all)
-{
-	this->fil_balance = 0;
-	this->balance_all = 0;
-	Setfil_balance(fil_balance);
-	Setbalance_all(balance_all);
-}
+uint Bank::balance_all = 500000;
 
-Bank::Bank(uint deposite, uint t_deposite, uint t_credit, uint g_credit,uint val,uint value)
-{
-	this->deposite = 0;
-	this->t_deposite = 0;
-	this->t_credit = 0;
-	this->g_credit = 0;
-	Setdeposite(deposite);
-	Sett_deposite(t_deposite,value);
-	Sett_credit(t_credit);
-	Setg_credit(g_credit, val);
-}
-
-void Bank::Setbalance_all(uint balance_all)
-{
-	this->balance_all = balance_all;
-}
 
 void Bank::Setfil_balance(uint fil_balance)
 {
@@ -34,44 +12,25 @@ void Bank::Setfil_balance(uint fil_balance)
 	else fil_balance = balance_all;
 }
 
-void Bank::Setwf_balance(uint wf_balance)
+void Bank::Deposite(uint deposite)
 {
-	if (wf_balance < fil_balance)
-		this->wf_balance = wf_balance;
-	else
-		wf_balance = fil_balance;
+		this->deposite = deposite;	
 }
 
-void Bank::Setwb_balance(uint wb_balance)
+void Bank::Credit(uint credit)
 {
-	if (wb_balance < balance_all)
-		this->wb_balance = wb_balance;
-	else
-		wb_balance = balance_all;
-}
-
-void Bank::Setdeposite(uint deposite)
-{
-	if (deposite < wf_balance)
-		this->deposite = deposite;
-	else
-		cout << "sorry we cant give u deposite :(";
-}
-
-void Bank::Sett_deposite(uint t_deposite, uint value)
-{
-	this->t_deposite += t_deposite*value / 100;
-}
-
-void Bank::Sett_credit(uint credit)
-{
-	if (t_credit < fil_balance || t_credit < balance_all)
-		this->t_credit = credit;
+	if (credit < fil_balance && credit < balance_all)
+		this->credit = credit;
 	else
 		cout << "sorry we can't give u credit :(";
 }
 
-void Bank::Setg_credit(uint g_credit, uint val)
+void Bank::Return_deposite(uint deposite, uint persent)
 {
-	this->g_credit += g_credit*val / 100;
+	this->deposite += deposite*persent / 100;
+}
+
+void Bank::Return_credit(uint credit, uint persent)
+{
+	this->credit += credit*persent / 100;
 }
