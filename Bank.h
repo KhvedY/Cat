bbaac252f41a1feded353aa2e4840617dd63ec53
KhvedY <jillfoh@mail.ru>
2016-02-08 02:32:25 +0300

    Update Bank.h

--- Bank.h
+++ Bank.h
@@ -1,5 +1,7 @@
 #pragma once
 #include "Bank.h"
+#pragma once
+#include "Bank.h"
 typedef unsigned int uint;
 
 
@@ -8,45 +10,32 @@ class Bank
 private:
 	uint fil_balance;
 	static uint balance_all;
-	uint wf_balance;
-	uint wb_balance;
+	uint persent;
 	uint deposite;
-	uint t_deposite;
-	uint t_credit;
-	uint g_credit;
-	uint val;
-	uint value;
+	uint credit;
+
 public:
 
 	void Setfil_balance(uint fil_balance);
 	uint Getfil_balance(){ return fil_balance; };
 
-	void Setbalance_all(uint balance_all);
-	uint Getbalance_all(){ return balance_all; };
-	
-	void Setwf_balance(uint wf_balance);
-	uint Getwf_balance(){ return wf_balance; };
+	static uint Getbalance_all(){ return balance_all; };
 
-	void Setwb_balance(uint wb_balance);
-	uint Getwb_balance(){ return wb_balance; };
+	void Deposite(uint deposite);
+	uint Deposite(){ return deposite; };
 
-	void Setdeposite(uint deposite);
-	uint Getdeposite(){ return deposite; };
+	void Return_deposite(uint deposite, uint persent);
+	uint GetReturn_deposite(){ return deposite; };
 
-	void Sett_deposite(uint t_deposite, uint value);
-	uint Gett_deposite(){ return t_deposite; };
+	void Credit(uint credit);
+	uint Credit(){ return credit; };
 
-	void Sett_credit(uint t_credit);
-	uint Gett_credit(){ return t_credit; };
+	void Return_credit(uint credit, uint persent);
+	uint GetReturn_credit(){ return credit; };
 
-	void Setg_credit(uint g_credit,uint val);
-	uint Getg_credit(){ return g_credit; };
-
-	
-	Bank() : Bank(0, 0){};
-	Bank(uint fil_balance, uint balance_all);
-	Bank():Bank(1, 2, 3, 0, 0, 0){};
-	Bank(uint wf_balance, uint wb_balance, uint deposite, uint t_deposite, uint t_credit, uint g_credit);
-	~Bank();
-	
+	Bank()
+	{
+		fil_balance = 10000;
+		balance_all += fil_balance;
+	}
 };
