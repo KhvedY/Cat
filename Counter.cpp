3b827f39da66686f487e9112a25e8ead02322d95
KhvedY <jillfoh@mail.ru>
2016-02-13 03:30:50 +0300

    Update Counter.cpp

--- Counter.cpp
+++ Counter.cpp
@@ -1,20 +1,50 @@
 #include "Counter.h"
 
-Counter::Counter(uint min_value, uint max_value, uint step)
+
+Counter::Counter(uint zeroing)
 {
-	this->min_value = 0;
-	this->max_value = 0;
-	this->step = 0;
-	Setmin_value(min_value);
-	Setmax_value(max_value);
-	Setstep(step);
+	this->zeroing = 0;
+	Setzeroing(zeroing);
 }
-	Counter::Counter(uint zeroing)
-	{
+Counter::Counter(uint min_value_Set, uint max_value_Set, uint step_grow, uint current_value)
+{
+	this->min_value_Set = 0;
+	this->max_value_Set = 0;
+	this->step_grow = 0;
+	this->current_value = 0;
+	Setmin_value_Set(min_value_Set);
+	Setmax_value_Set(max_value_Set);
+	Setstep_grow(step_grow);
+	Setcurrent_value(current_value);
+}
+
+void Counter::Setcurrent_value(uint current_value)
+{
+	if (current_value<=max_value&&current_value<=max_value_Set)
+		this->current_value = current_value;
+	else
+		std::cout << " Too HIGH value of counter !" << std::endl;
+}
+
+void Counter::Setstep_grow(uint step_grow)
+{
+	this->step_grow = step_grow+current_value;
+}
+
+void Counter::Setzeroing(uint zeroing)
+{
+	if (zeroing > 99999)
 		this->zeroing = 0;
-		Setzeroing(zeroing);
+	else
+	this->zeroing = zeroing;
 }
-	Counter::Counter(uint max_val, uint min_val, uint step_set, uint current_value)
-	{
-		this->max_val = 0;
-	}
+
+void Counter::Setmin_value_Set(uint min_value_Set)
+{
+	this->min_value_Set = min_value_Set;
+}
+
+void Counter::Setmax_value_Set(uint max_value_Set)
+{
+	this->max_value_Set = max_value_Set;
+}
