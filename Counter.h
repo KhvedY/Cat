4dfdcc5d5a1e1c901f5ade5c1709fc4e5ccaa5dc
KhvedY <jillfoh@mail.ru>
2016-02-16 20:06:34 +0300

    Update Counter.h

--- Counter.h
+++ Counter.h
@@ -9,35 +9,30 @@ private:
 
 	uint current_value;
 	uint min_value;
-        uint max_value;
-        uint step;
-	uint step_grow;
-	uint zeroing;
-
+	uint max_value;
+	uint step;
+	
 public:
 
 	void Setcurrent_value(uint current_value);
 	uint Getcurrent_value(){ return current_value; };
 
 	void Setmin_value(uint min_value);
-	 uint Getmin_value(){ return min_value; };
+	uint Getmin_value(){ return min_value; };
 
-	 void Setmax_value(uint max_value);
-	 uint Getmax_value(){ return max_value; };
+	void Setmax_value(uint max_value);
+	uint Getmax_value(){ return max_value; };
 
-	 void Setstep(uint step);
+	void Setstep(uint step);
 	uint Getstep(){ return step; };
 
-	void Setstep_grow(uint step_grow);
-	uint Getstep_grow(){ return step_grow; };
-
-	void Setzeroing(uint zeroing);
-	uint Getzeroing(){ return zeroing; };
-
-	Counter(){};
-
+	
 	Counter(uint min_value, uint max_value, uint step, uint current_value);
-
-	Counter(uint zeroing);
-
+	Counter()
+	{ 
+		min_value = 0;
+		max_value = 99999;
+		step = 1;
+		current_value = 0;
+	};
 };
