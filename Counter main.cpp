1a20347ccc1dadb0a88456ca637c71e6198fbece
KhvedY <jillfoh@mail.ru>
2016-02-16 20:08:04 +0300

    Update Counter main.cpp

--- Counter main.cpp
+++ Counter main.cpp
@@ -3,17 +3,12 @@
 void main()
 {
 	Counter f;
-	f.Setcurrent_value(1134);
+	f.Setcurrent_value(12345);
 	std::cout << "The current value of counter is :" << f.Getcurrent_value() << std::endl;
 	f.Setmin_value(0);
 	std::cout << "Minimal value of counter is :" << f.Getmin_value() << std::endl;
 	f.Setmax_value(99999);
 	std::cout << "Maximal value of counter is :" << f.Getmax_value() << std::endl;
-	f.Setstep(0);
+	f.Setstep(10);
 	std::cout << "Step of counter is :" << f.Getstep() << std::endl;
-	f.Setstep_grow(10);
-	std::cout << "Currently step of counter is :" << f.Getstep_grow() << std::endl;
-	f.Setzeroing(100000);
-	std::cout << "Current value of counter is :" << f.Getzeroing() << std::endl;
-
 }
