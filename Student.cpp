5349fdf8d558b9e1427330c4d24033cc174069df
KhvedY <jillfoh@mail.ru>
2016-02-28 14:08:49 +0300

    Update Student.cpp

--- Student.cpp
+++ Student.cpp
@@ -1,8 +1,8 @@
 #include "Student.h"
 
-Student::Student(char* surname, char* name, char* patronymic, char* adress, uint telephone_number, uint day, uint month, uint year)
+Student::Student(char* surname, char* name, char* patronymic, char* adress, char* telephone_number, uint day, uint month, uint year)
 {
-	
+
 	this->Name.surname = nullptr;
 	this->Name.name = nullptr;
 	this->Name.patronymic = nullptr;
@@ -17,7 +17,7 @@ Student::Student(char* surname, char* name, char* patronymic, char* adress, uint
 	Setyear(year);
 }
 
-Student::Student()
+Student::~Student()
 {
 	delete[]Name.surname;
 	delete[]Name.name;
@@ -25,25 +25,86 @@ Student::Student()
 	delete[]p_data.adress;
 }
 
-Student::Student(uint* credit, uint* exams, uint* course_works)
-{
-	this->mark.credit = nullptr;
-	this->mark.exams = nullptr;
-	this->mark.course_works = nullptr;
-	Setcredit(credit);
-	Setexams(exams);
-	Setcourse_works(course_works);
-}
 
-Student::Student()
-{
-	delete[]mark.credit;
-	delete[]mark.exams;
-	delete[]mark.course_works;
-}
-
-void Setday(uint day)
+void Student::Setday(uint day)
 {
 	if (day < 31)
-    birthday.Day = day;
+		this->birthday.Day = day;
+}
+
+void Student::Setmonth(uint month)
+{
+	if (month > 0 && month < 13)
+		this->birthday.Month = month;
+}
+
+void Student::Setyear(uint year)
+{
+	this->birthday.Year = year;
+}
+
+void Student::Setsurname(char* surname)
+{
+	if (this->Name.surname)
+		delete[]this->Name.surname;
+	this->Name.surname = new char(strlen(surname + 1));
+	strcpy_s(this->Name.surname, strlen(surname + 1), surname);
+}
+
+void Student::Setname(char* name)
+{
+	if (this->Name.name)
+		delete[]this->Name.name;
+	this->Name.name = new char(strlen(name + 1));
+	strcpy_s(this->Name.name, strlen(name + 1), name);
+}
+
+void Student::Setpatronymic(char* patronymic)
+{
+	if (this->Name.patronymic)
+		delete[]this->Name.patronymic;
+	this->Name.patronymic = new char(strlen(patronymic + 1));
+	strcpy_s(this->Name.patronymic, strlen(patronymic + 1), patronymic);
+}
+
+void Student::Setcredit(uint SomeMarks)
+{
+	SetMarks(mark.credit, mark.c_size, SomeMarks);
+}
+
+void Student::Setexams(uint SomeMarks)
+{
+	SetMarks(mark.exams, mark.e_size, SomeMarks);
+}
+
+void Student::Setcourse_works(uint SomeMarks)
+{
+	SetMarks(mark.course_works, mark.cw_size, SomeMarks);
+}
+
+void Student::Setadress(char* adress)
+{
+	if (this->p_data.adress)
+		delete[]this->p_data.adress;
+	this->p_data.adress = new char(strlen(adress + 1));
+	strcpy_s(this->p_data.adress, strlen(adress + 1), adress);
+}
+
+void Student::Settelephone_number(char* telephone_number)
+{
+	if (this->p_data.telephone_number)
+		delete[]this->p_data.telephone_number;
+	this->p_data.telephone_number = new char(strlen(telephone_number + 1));
+	strcpy_s(this->p_data.telephone_number, strlen(telephone_number + 1), telephone_number);
+}
+
+void Student::SetMarks(uint*&arr, uint &size, uint SomeMarks)
+{
+	uint*temp = new uint[size + 1];
+	for (int i = 0; i < size; i++)
+		temp[i] = arr[i];
+	temp[size] = SomeMarks;
+	delete[]arr;
+	arr = temp;
+	size++;
 }
