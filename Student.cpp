7880119f9b101e551c82b54d0d9f91eb08012126
KhvedY <jillfoh@mail.ru>
2016-03-04 21:23:18 +0200

    Update Student.cpp

--- Student.cpp
+++ Student.cpp
@@ -1,3 +1,4 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include "Student.h"
 
 Student::Student(char* surname, char* name, char* patronymic, char* adress, char* telephone_number, uint day, uint month, uint year)
@@ -7,22 +8,27 @@ Student::Student(char* surname, char* name, char* patronymic, char* adress, char
 	this->Name.name = nullptr;
 	this->Name.patronymic = nullptr;
 	this->p_data.adress = nullptr;
+	this->p_data.telephone_number = nullptr;
 	Setsurname(surname);
-	Setname(name);
-	Setpatronymic(patronymic);
-	Setadress(adress);
-	Settelephone_number(telephone_number);
-	Setday(day);
-	Setmonth(month);
-	Setyear(year);
+	//Setname(name);
+	//Setpatronymic(patronymic);
+	//Setadress(adress);
+	//Settelephone_number(telephone_number);
+	//Setday(day);
+	//Setmonth(month);
+	//Setyear(year);
 }
 
 Student::~Student()
 {
-	delete[]Name.surname;
-	delete[]Name.name;
+	//if (Name.surname) delete[]Name.surname;
+	/*delete[]Name.name;
 	delete[]Name.patronymic;
-	delete[]p_data.adress;
+	if (p_data.adress) delete[]p_data.adress;
+	if (p_data.telephone_number) delete[]p_data.telephone_number;
+	if (mark.credit) delete[]mark.credit;
+	if (mark.course_works) delete[]mark.course_works;
+	if (mark.exams) delete[]mark.exams;*/
 }
 
 
@@ -47,24 +53,27 @@ void Student::Setsurname(char* surname)
 {
 	if (this->Name.surname)
 		delete[]this->Name.surname;
-	this->Name.surname = new char(strlen(surname + 1));
-	strcpy_s(this->Name.surname, strlen(surname + 1), surname);
+	uint size = strlen(surname);
+	this->Name.surname = new char(size + 1);
+	strcpy_s(this->Name.surname, size + 1, surname);
 }
 
 void Student::Setname(char* name)
 {
 	if (this->Name.name)
 		delete[]this->Name.name;
-	this->Name.name = new char(strlen(name + 1));
-	strcpy_s(this->Name.name, strlen(name + 1), name);
+	uint size = strlen(name);
+	this->Name.name = new char(size + 1);
+	strcpy_s(this->Name.name, size + 1, name);
 }
 
 void Student::Setpatronymic(char* patronymic)
 {
 	if (this->Name.patronymic)
 		delete[]this->Name.patronymic;
-	this->Name.patronymic = new char(strlen(patronymic + 1));
-	strcpy_s(this->Name.patronymic, strlen(patronymic + 1), patronymic);
+	uint size = strlen(patronymic);
+	this->Name.patronymic = new char(size + 1);
+	strcpy_s(this->Name.patronymic, size + 1, patronymic);
 }
 
 void Student::Setcredit(uint SomeMarks)
@@ -72,39 +81,39 @@ void Student::Setcredit(uint SomeMarks)
 	SetMarks(mark.credit, mark.c_size, SomeMarks);
 }
 
-void Student::Setexams(uint SomeMarks)
-{
-	SetMarks(mark.exams, mark.e_size, SomeMarks);
-}
-
-void Student::Setcourse_works(uint SomeMarks)
-{
-	SetMarks(mark.course_works, mark.cw_size, SomeMarks);
-}
+//void Student::Setexams(uint SomeMarks)
+//{
+//	SetMarks(mark.exams, mark.e_size, SomeMarks);
+//}
+//
+//void Student::Setcourse_works(uint SomeMarks)
+//{
+//	SetMarks(mark.course_works, mark.cw_size, SomeMarks);
+//}
 
 void Student::Setadress(char* adress)
 {
 	if (this->p_data.adress)
 		delete[]this->p_data.adress;
-	this->p_data.adress = new char(strlen(adress + 1));
-	strcpy_s(this->p_data.adress, strlen(adress + 1), adress);
+	this->p_data.adress = new char(strlen(adress) + 1);
+	strcpy_s(this->p_data.adress, strlen(adress) + 1, adress);
 }
 
 void Student::Settelephone_number(char* telephone_number)
 {
 	if (this->p_data.telephone_number)
 		delete[]this->p_data.telephone_number;
-	this->p_data.telephone_number = new char(strlen(telephone_number + 1));
-	strcpy_s(this->p_data.telephone_number, strlen(telephone_number + 1), telephone_number);
+	this->p_data.telephone_number = new char(strlen(telephone_number) + 1);
+	strcpy_s(this->p_data.telephone_number, strlen(telephone_number) + 1, telephone_number);
 }
 
 void Student::SetMarks(uint*&arr, uint &size, uint SomeMarks)
 {
-	uint*temp = new uint[size + 1];
+	uint *temp = new uint[size + 1];
 	for (int i = 0; i < size; i++)
 		temp[i] = arr[i];
 	temp[size] = SomeMarks;
-	delete[]arr;
+	if(arr)delete[]arr;
 	arr = temp;
 	size++;
 }
