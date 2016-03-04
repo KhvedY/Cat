411972aa1ca8ccedd795541a4b6793cf566aff8f
KhvedY <jillfoh@mail.ru>
2016-03-04 21:23:43 +0200

    Update main Student.cpp

--- main Student.cpp
+++ main Student.cpp
@@ -1,16 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include "Student.h"
 #include <iostream>
 using namespace std;
 
 void main()
 {
-	Student* a = new Student("Petrov","Petr","Petrovich","Konnaya 2","095659123",21,12,1992);
-	a->Setcredit(9);
+	Student a("Petrov", "Petr", "Petrovich", "Konnaya 2", "095659123", 21, 12, 1992);
+	a.Setcredit(9); 
+	a.Setcredit(15);/*
 	a->Setexams(10);
-	a->Setcourse_works(11);
+	a->Setcourse_works(11);*/
+	cout << a.Getcredit(1);
+	cout << a.Getcredit(2);
 
-	a->Get_birthday();
-	a->Get_mark();
-	a->Get_name();
-	a->Get_p_data();
+	//a->Get_birthday();
+	//a->Get_mark();
+	//a->Get_name();
+	//a->Get_p_data();
 }
