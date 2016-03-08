7b63157941b4ddd726e56d32bbe695201e9aa037
KhvedY <jillfoh@mail.ru>
2016-03-08 20:11:08 +0300

    Update main Student.cpp

--- main Student.cpp
+++ main Student.cpp
@@ -4,19 +4,19 @@ using namespace std;
 
 void main()
 {
-	Student a("Petrov", "Petr", "Petrovich", "Konnaya 2", "095659123", 21, 12, 1992);
+	Student a("Ivanov", "Ivan", "Ivanovich", "Konnaya 2", "095659123", 21, 12, 1992);
 	a.Setcredit(9);
 	a.Setexams(8);
 	a.Setcourse_works(7);
-	cout << a.Getsurname()<<endl;
-	cout << a.Getname()<<endl;
-	cout << a.Getpatronymic()<<endl;
-	cout << a.Getadress()<<endl;
-	cout << a.Gettelephone_number()<<endl;
-	cout << a.Getday()<<endl;
-	cout << a.Getmonth()<<endl;
-	cout << a.Getyear()<<endl;
+	cout << a.Getsurname() << endl;
+	cout << a.Getname() << endl;
+	cout << a.Getpatronymic() << endl;
+	cout << a.Getadress() << endl;
+	cout << a.Gettelephone_number() << endl;
+	cout << a.Getday() << endl;
+	cout << a.Getmonth() << endl;
+	cout << a.Getyear() << endl;
 	cout << a.Getcredit(1) << endl;;
-	cout << a.Getexams(1)<<endl;
-	cout << a.Getcourse_works(1)<<endl;	
+	cout << a.Getexams(1) << endl;
+	cout << a.Getcourse_works(1) << endl;
 }
